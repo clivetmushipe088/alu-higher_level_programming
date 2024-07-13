7550abbd62b0db126ac1c0624da34426b73075eb
clivetmushipe088 <c.mushipe@alustudent.com>
2024-07-13 08:13:22 -0700

    added

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    # Initialize counters
     count = 0  # To count the number of elements in my_list
-    printed_elements = 0  # To count how many elements are actually printed
-
-    # Count the number of elements in my_list without using len()
+    printed_elements = 0  # To count how many elements are printed
+    
+    # Determine the number of elements in my_list
     try:
         for _ in my_list:
             count += 1
     except TypeError:
-        # If my_list is not iterable, return 0 (this case is unlikely with the default value)
+        # If my_list is not iterable, return 0
         return 0
-
+    
     # Attempt to print up to x elements from my_list
     for i in range(x):
         try:
+            # Attempt to access and print the ith element
             print(my_list[i], end=' ')
             printed_elements += 1
         except IndexError:
-            # If x is greater than the number of elements, stop printing
+            # If an IndexError occurs, break the loop
             break
-
+    
     print()  # Move to a new line after printing
     return printed_elements
 
