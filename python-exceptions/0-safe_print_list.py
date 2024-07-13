cd94a9ffbc3eb93d5347516182e1015fed885fa9
clivetmushipe088 <c.mushipe@alustudent.com>
2024-07-13 08:22:55 -0700

    added

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    printed_elements = 0
+    count = 0  # To count the number of elements in my_list
+    printed_elements = 0  # To count how many elements are actually printed
     
-    # Determine the number of elements in my_list
+    # Determine the number of elements in 
     try:
         for _ in my_list:
             count += 1
@@ -13,19 +13,19 @@ def safe_print_list(my_list=[], x=0):
     # Print up to x elements
     for i in range(x):
         try:
-            print(my_list[i], end=' ')
+            print(my_list[i], end='')
             printed_elements += 1
         except IndexError:
             break  # Stop printing if there are no more elements to print
     
-    print()  # Print a newline after printing all elements
+    print()  # Move to a new line after printing all elements
     return printed_elements
 
 # Test cases
-print(safe_print_list([1, 2, 3, 4], 4))  # Expected output: 1 2 3 4\n (4)
-print(safe_print_list([1, 2, 3, 4], 2))  # Expected output: 1 2\n (2)
+print(safe_print_list([1, 2, 3, 4], 4))  # Expected output: 1234\n (4)
+print(safe_print_list([1, 2, 3, 4], 2))  # Expected output: 12\n (2)
 print(safe_print_list([1, 2, 3, 4], 0))  # Expected output: \n (0)
 print(safe_print_list([], 0))             # Expected output: \n (0)
-print(safe_print_list([1, 2, 3, 4], 5))  # Expected output: 1 2 3 4\n (4)
-print(safe_print_list([1, 2, 3, 4], 14)) # Expected output: 1 2 3 4\n (4)
+print(safe_print_list([1, 2, 3, 4], 5))  # Expected output: 1234\n (4)
+print(safe_print_list([1, 2, 3, 4], 14)) # Expected output: 1234\n (4)
 
