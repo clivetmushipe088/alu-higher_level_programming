920460b62bacf12399093ab1168f2c235f1b5cab
clivetmushipe088 <c.mushipe@alustudent.com>
2024-07-23 08:19:15 -0700

    added

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
+from add_0 import add
 
-    a = 1
-    b = 2
-    print(f"{a} + {b} = {result}")
+a = 1
+b = 2
+print(f"{a} + {b} = {result}")
 
