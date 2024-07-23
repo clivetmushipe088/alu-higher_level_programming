5ae1741fc0cc361a659c74ea6c873be91112baeb
clivetmushipe088 <c.mushipe@alustudent.com>
2024-07-23 08:29:53 -0700

    added

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    argc = len(argv) - 1
+
+    if argc == 0:
+        print("Number of argument(s): 0.")
+    else:
+        print("Number of argument(s): {} argument{}".format(argc, "" if argc == 1 else "s"))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, argv[i]))
+
