eaa25b53126105affba597eb28306713d15edc4b
clivetmushipe088 <c.mushipe@alustudent.com>
2024-07-23 08:14:44 -0700

    added

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# Ensure this code runs only when the script is executed directly, not when imported
 if __name__ == "__main__":
     from add_0 import add
 
     a = 1
     b = 2
-    result = add(a, b)
     print(f"{a} + {b} = {result}")
 
