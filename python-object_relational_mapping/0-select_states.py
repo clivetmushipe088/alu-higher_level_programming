1c6bd5c66c996d69b664c98984a07ab7605374a4
clivetmushipe088 <c.mushipe@alustudent.com>
2024-08-08 22:13:46 +0000

    added

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import MySQLdb, sys; db=MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]); c=db.cursor(); c.execute("SELECT * FROM states ORDER BY id"); [print(row) for row in c.fetchall()]
 if __name__ == "__main__": pass
 
