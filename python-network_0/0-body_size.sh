3dda075b9f47b72b0148036bf19d00eee8151ed2
clivetmushipe088 <c.mushipe@alustudent.com>
2024-08-08 21:39:21 +0000

    added

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-\\h\\
-heck if a URL was provided as an argument
+# Check if a URL was provided as an argument
 if [ -z "$1" ]; then
   echo "Usage: $0 <URL>"
   exit 1
 fi
-
 # Use curl to send a request to the URL and get the size of the body in bytes
 size=$(curl -s -o /dev/null -w '%{size_download}' "$1")
 
